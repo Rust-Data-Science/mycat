770c694c52e0bec36b06a6d27a7edbf210afad77
tushushu <xmaseve1984@gmail.com>
2022-03-29 15:49:48 +0800

    info methods

--- mycat/frame.py
+++ mycat/frame.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple, List
 
 from ulist import UltraFastList
 
@@ -13,3 +13,32 @@ class DataFrame:
 
     def __init__(self, data: Dict[str, UltraFastList]) -> None:
         self._data = data
+
+    @property
+    def columns(self) -> List[str]:
+        """The column labels of self."""
+        return list(self._data.keys())
+
+    @property
+    def empty(self) -> bool:
+        """Whether self is empty."""
+        if self.n_cols == 0 or self.n_rows == 0:
+            return True
+        return False 
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        """The dimensionality of self."""
+        return (self.n_rows, self.n_cols)
+
+    @property
+    def n_rows(self) -> int:
+        """The number of rows in self."""
+        if self.n_cols == 0:
+            return 0
+        return len(list(self._data.values())[0])
+
+    @property
+    def n_cols(self) -> int:
+        """The number of columns in self."""
+        return len(self._data)
