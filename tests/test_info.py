165ad0af8d3abf15c0115fefe62ffe3f65d0e520
tushushu <m18591915960@163.com>
2022-04-04 17:26:33 +0800

    more tests

--- tests/test_info.py
+++ tests/test_info.py
@@ -11,6 +11,12 @@ DATAFRAMES = {
         }
     ),
     'df2': mc.DataFrame(dict()),
+    'df3': mc.DataFrame(
+        {
+            'foo': ul.from_seq([], dtype='int'),
+            'bar': ul.from_seq([], dtype='bool'),
+        }
+    ),
 }
 
 
@@ -18,10 +24,24 @@ DATAFRAMES = {
     'method, df, expected',
     [
         ('columns', 'df1', ['foo', 'bar']),
+        ('columns', 'df2', []),
+        ('columns', 'df3', ['foo', 'bar']),
+
         ('empty', 'df1', False),
+        ('empty', 'df2', True),
+        ('empty', 'df3', True),
+
         ('shape', 'df1', (3, 2)),
+        ('shape', 'df2', (0, 0)),
+        ('shape', 'df3', (0, 2)),
+
         ('n_rows', 'df1', 3),
+        ('n_rows', 'df2', 0),
+        ('n_rows', 'df3', 0),
+
         ('n_cols', 'df1', 2),
+        ('n_cols', 'df2', 0),
+        ('n_cols', 'df3', 2),
     ],
 )
 def test(method: str, df: str, expected: Any) -> None:
